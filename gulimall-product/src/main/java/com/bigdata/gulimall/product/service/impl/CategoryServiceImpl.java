be73fb4a64c3b388ecca345a2b956a9771ab09e5
w-chen <2499641477@qq.com>
2022-08-23 01:31:53 +0800

    项目基础篇提交

---
+++ gulimall-product/src/main/java/com/bigdata/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,46 @@
+package com.bigdata.gulimall.product.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.bigdata.common.utils.PageUtils;
+import com.bigdata.common.utils.Query;
+
+import com.bigdata.gulimall.product.dao.CategoryDao;
+import com.bigdata.gulimall.product.entity.CategoryEntity;
+import com.bigdata.gulimall.product.service.CategoryService;
+
+
+@Service("categoryService")
+public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
+
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        IPage<CategoryEntity> page = this.page(
+                new Query<CategoryEntity>().getPage(params),
+                new QueryWrapper<CategoryEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        List<CategoryEntity> categoryEntities = categoryDao.selectList(null);
+        return categoryEntities;
+    }
+
+    @Override
+    public   void removeMenuByIds(List<Long> asList) {
+        //TODO 检查当前的菜单是否被别的地方所引用
+        categoryDao.deleteBatchIds(asList);
+    }
+}
